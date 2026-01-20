3adc89d0fe6fa2cca29a8ed44f7c3f01c5d42ea5
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-01-20 11:29:43 +0530

    Random Quotes Generator

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-//Random Paper Generator
+//Random Quotes Generator
 
-let main = document.querySelector("main")
-let button = document.querySelector("button")
+    let main = document.querySelector("main")
+    let button = document.querySelector("button")
 
 button.addEventListener("click",function(){
-    let div = document.createElement("div")
+    let h1 = document.createElement("h1")
     let c1 = Math.floor(Math.random()*256)
     let c2 = Math.floor(Math.random()*256)
     let c3 = Math.floor(Math.random()*256)
+    
 
-    div.style.height = "150px"
-    div.style.width = "150px"
-    div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
-    main.appendChild(div)
+    let arr = ["Learn. Code. Repeat.","One bug at a time.","Progress > Perfection.","Turning coffee into code","From errors to excellence.","Every line of code counts."]
+
+    let rad = (Math.floor(Math.random()*arr.length))
+    h1.innerHTML = arr[rad]
+    h1.style.color = `rgb(${c1},${c2},${c3})`
+    h1.style.position = "absolute"
+    h1.style.scale = (Math.floor(Math.random()*3))
+    main.append(h1)
+    
+//  To create random position of the quotes on the screen
 
     let x =(Math.floor(Math.random()*100))
     let y = (Math.floor(Math.random()*100))
-    div.style.position = "absolute"  
+      
+    h1.style.left = x + "%"
+    h1.style.top = y + "%"
     
-    div.style.left = x + "%"
-    div.style.top = y + "%"
     let c4 = Math.floor(Math.random()*180)
-    div.style.rotate = c4 + "deg"
+    h1.style.rotate = c4 + "deg"
     
 })
