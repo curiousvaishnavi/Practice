5456c368efac85e00de9429caed888516e11b073
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-01-23 23:48:12 +0530

    Instagram Like Feature

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-let button = document.querySelector("button")
-let inner = document.querySelector("#inner")
-let h1 = document.querySelector("h1")
-let card = document.querySelector("#card")
-let container = document.querySelector(".container")
-let grow = 0
+let img = document.querySelector("#pic");
+let love = document.querySelector("#love");
 
-
-button.addEventListener("click",function(){
-   
-    button.style.pointerEvents = "none"
-
-    let int = 50 + Math.floor(Math.random()* 50)
+img.addEventListener("dblclick", function () {
     
-    //Progress bar grow logic
-    let num = setInterval(function(){
-        grow++
-        h1.innerHTML = grow + "%"
-        inner.style.width = grow + "%"
-        
-    },int)
-     
-    //Progress bar must stop on 100% logic
-    setTimeout(function(){
-        clearTimeout(num)
-        button.innerHTML = "Downloaded"
-        button.style.pointerEvents = "none"
-        button.style.opacity = 0.5;   
-        button.style.cursor = "not-allowed"
-        card.style.userSelect = "none"
-        console.log("Downloaded in "+`${int/10}`+"seconds")
-    },int * 100)
+    // show heart
+    love.style.opacity = 1;
+    love.style.transform = "translate(-50%, -50%) scale(1.5)";
 
-})
+    // hide after some time
+    setTimeout(() => {
+        love.style.opacity = 0;
+        love.style.transform = "translate(-50%, -50%) scale(0) rotate(-60deg)";
+    }, 800);
+
+});
+
+
+
+
+
